17cb841e8da80a6e24a36b97e9ed032a5d5bcc89
Magdalena Wójcik <magdalena.wojcik@gmail.com>
2014-05-12 21:55:34 +0200

    Added matching original iris classes to new ones and testing

--- cluster.py
+++ cluster.py
@@ -4,10 +4,6 @@ import data_loader
 import data_writer
 import numpy as np
 
-data = data_loader.read_data('iris.data', skip_class=True, skip_header=False)
-data = np.array(data)
-data = data.astype(np.float)
-
 
 def k_means(data, k):
 
@@ -45,7 +41,7 @@ def k_means(data, k):
         for x in range(means.shape[0]):
             means[x] = means[x] / (classes == x).sum()
 
-    return classes
+    return (means, classes)
 
 
 def k_nn(data, data_classes, k, element):
@@ -73,46 +69,125 @@ def k_nn(data, data_classes, k, element):
     return np.argmax(classes)
 
 
-k_m = 0
-k_n = 10000
+def k_means_test(data, means, classes, org_classes):
 
-while(k_m < 2 or k_m >= data.shape[0] / 3):
-    
-    k_m = raw_input("Input number of k-means: ")
-    k_m = int(k_m)
-    if k_m < 2:
-        print "K too small. Try k > 1"
-    elif k_m >= data.shape[0] / 3:
-        print "K too large. Try k < " + str(data.shape[0] / 3)
+    org_means = np.zeros((3, data.shape[1]))
+    ans = np.zeros((data.shape[0], 1))
+    ans = ans.astype(np.integer)
 
-new_classes = k_means(data, k_m)
-data_writer.write_data('out.data', "K-means", k_m, data, new_classes)
-print("Output of k-Means written to out.data file")
+    for x in range(org_classes.shape[0]):
+        if org_classes[x] == "Iris-setosa":
+            ans[x] = 0
+        elif org_classes[x] == "Iris-versicolor":
+            ans[x] = 1
+        elif org_classes[x] == "Iris-virginica":
+            ans[x] = 2
 
-while(k_n >= data.shape[0]):
-    
-    k_n = raw_input("Input number of k-NN: ")
-    k_n = int(k_n)
-    if k_n >= data.shape[0]:
-        print "K too large. Try k < " + str(data.shape[0])
+    for x in range(data.shape[0]):
+        for y in range(data.shape[1]):
+            org_means[ans[x], y] += data[x, y]
 
-new_element = []
-coo = 0
+    for x in range(org_means.shape[0]):
+        org_means[x] = org_means[x] / (ans == x).sum()
 
-print("Add coordinates of the new element")
+    distances = np.zeros((org_means.shape[0], 3))
+    mappings = np.zeros((org_means.shape[0], 1))
 
-for x in range(data.shape[1]):
-    
-    coo = raw_input("Enter float for " + str(x + 1) + ". coordinate: ")
-    if coo == "":
-        coo = 0
-    coo = float(coo)    
-    new_element.append(coo)
+    for x in range(means.shape[0]):
+        for y in range(org_means.shape[0]):
+            diff = means[x] - org_means[y]
+            dist = sum(diff**2)
+            dist = np.sqrt(dist)
+            distances[x, y] = dist
+        mappings[x] = np.argmin(distances[x])
 
-new_element = np.array([new_element])
-new_class = k_nn(data, new_classes, k_n, new_element)
-new_class = np.array([[new_class]])
+    mappings = mappings.astype(np.string0)
 
-data_writer.write_data('out.data', "K-NN", k_n, new_element, new_class)
+    for x in range(mappings.shape[0]):
+        if mappings[x] == '0.0':
+            mappings[x] = "Iris-setosa"
+        elif mappings[x] == '1.0':
+            mappings[x] = "Iris-versicolor"
+        elif mappings[x] == '2.0':
+            mappings[x] = "Iris-virginica"
 
-print("Output of k-NN written to out.data file")
+    new_classes = np.empty((classes.shape[0], 1), dtype=np.dtype('a25'))
+
+    for x in range(classes.shape[0]):
+        new_classes[x] = mappings[classes[x]]
+
+    return (mappings, new_classes)
+
+
+def interact():
+
+    skip_class = False
+
+    data = data_loader.read_data('in.data', skip_class=skip_class,
+                                 skip_header=False)
+    data = np.array(data)
+
+    if not skip_class:
+        split = np.split(data, [-1], axis=1)
+        data = split[0]
+        org_classes = split[1]
+
+    data = data.astype(np.float)
+
+    k_m = 0
+    k_n = 10000
+
+    while(k_m < 2 or k_m >= data.shape[0] / 3):
+
+        k_m = raw_input("Input number of k-means: ")
+        k_m = int(k_m)
+        if k_m < 2:
+            print "K too small. Try k > 1"
+        elif k_m >= data.shape[0] / 3:
+            print "K too large. Try k < " + str(data.shape[0] / 3)
+
+    means, new_classes = k_means(data, k_m)
+
+    if k_m == 3:
+        mappings, coded_classes = k_means_test(data, means, new_classes,
+                                               org_classes)
+        data_writer.write_tests('out.data', "K-means", k_m, data,
+                                coded_classes, org_classes)
+        print("Output of k-Means with test written to out.data file")
+    else:
+        data_writer.write_data('out.data', "K-means", k_m, data, new_classes)
+        print("Output of k-Means written to out.data file")
+
+    while(k_n >= data.shape[0]):
+
+        k_n = raw_input("Input number of k-NN: ")
+        k_n = int(k_n)
+        if k_n >= data.shape[0]:
+            print "K too large. Try k < " + str(data.shape[0])
+
+    new_element = []
+    coo = 0
+
+    print("Add coordinates of the new element")
+
+    for x in range(data.shape[1]):
+
+        coo = raw_input("Enter float for " + str(x + 1) + ". coordinate: ")
+        if coo == "":
+            coo = 0
+        coo = float(coo)
+        new_element.append(coo)
+
+    new_element = np.array([new_element])
+    new_class = k_nn(data, new_classes, k_n, new_element)
+
+    if k_m == 3:
+        new_class = mappings[new_class]
+
+    new_class = np.array([[new_class]])
+
+    data_writer.write_data('out.data', "K-NN", k_n, new_element, new_class)
+
+    print("Output of k-NN written to out.data file")
+
+interact()
--- data_writer.py
+++ data_writer.py
@@ -11,6 +11,17 @@ def write_data(filename, algorithm, k, data, classes):
     with open(filename, attr) as f:
         f.write("Output of " + algorithm + ", with k=" + str(k) + "\n\n")
         for x in range(data.shape[0]):
-            f.write(str(data[x]) + " class: " + str(classes[x]) + "\n")
+            f.write(str(data[x]) + " calculated class: " + str(classes[x]) + "\n")
+
+        f.write("\n\n")
+
+
+def write_tests(filename, algorithm, k, data, classes, org_classes):
+
+    with open(filename, 'w') as f:
+        f.write("Output of " + algorithm + ", with k=" + str(k) + "\n\n")
+        for x in range(data.shape[0]):
+            f.write(str(data[x]) + " calculated class: " + str(classes[x]))
+            f.write(" original class: " + str(org_classes[x]) + "\n")
 
         f.write("\n\n")
